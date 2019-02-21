eb42e0f5416363c4a673d26f83866fa0c65cbabd
LAMM STUDIOS <martosmoralluisalberto@gmail.com>
2019-02-21 21:16:12 +0100

    Commit 2.1 de c++

--- test1.cpp
+++ test1.cpp
@@ -6,10 +6,25 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+
+	float x = 10.5;
+	float y = 11.8;
+	float z = 3.3;
+
 	cout << "Hola Mundo\n";
 	cout << "Este es mi primer programa en c++\n";
 	system("PAUSE");
 	cout << "Ahora pondre operaciones de arimetica y una clase .h\n";
 	system("PAUSE");
+
+	if (x + y + z)
+	{
+		cout << "La suma es:\n" <<x+y+z << "\n";
+	}
+	else{
+		printf("Ha habido un error en la operación");
+	}
+	system("PAUSE");
+
 	return 0;
 }
