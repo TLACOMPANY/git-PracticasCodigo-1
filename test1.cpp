b4489a86af20e5a9dbfb46c78ca8cf710b24dd69
LAMM STUDIOS <martosmoralluisalberto@gmail.com>
2019-02-21 21:04:15 +0100

    2º Commit de la rama Login

---
+++ test1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	cout << "Hola Mundo\n";
+	cout << "Este es mi primer programa en c++\n";
+	system("PAUSE");
+	cout << "Ahora pondre operaciones de arimetica y una clase .h\n";
+	system("PAUSE");
+	return 0;
+}
